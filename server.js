e5842afcafc46d796f362894ae296380114cee6d
unknown <Johnnybravo23>
2019-06-07 16:39:16 -0700

    prep

--- server.js
+++ server.js
@@ -8,17 +8,6 @@ const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
 const posts = require('./routes/api/posts');
 
-// server static assets if in production
-if(process.env.NODE_ENV === 'production') {
-  // set static folder
-  app.use(express.static('client/build'));
-  // creating the route
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
-
-// initilization
 const app = express();
 
 // Body parser middleware
@@ -45,6 +34,16 @@ app.use('/api/users', users);
 app.use('/api/profile', profile);
 app.use('/api/posts', posts);
 
-const port = process.env.PORT || 8080;
+// Server static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
